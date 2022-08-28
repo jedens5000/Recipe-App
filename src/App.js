862c3bf8e2a8b4b5f2861de10d0e5ab3b5b9c9d9
jedens5000 <70732210+jedens5000@users.noreply.github.com>
2022-08-28 13:03:28 -0400

    Spoonacular API Branch created

--- src/App.js
+++ src/App.js
@@ -4,21 +4,40 @@ import Recipe from "./Recipe";
 import "./App.css";
 
 const App = () => {
-  const APP_ID = "c47ed24d";
-  const APP_KEY = "33938f91dcf9533e554a4e7e1a107347";
+  // const APP_ID = "c47ed24d";
+  // const APP_KEY = "33938f91dcf9533e554a4e7e1a107347";
+  const SPOON_KEY = "35e995f44c4145188a3b2482b4785a96";
 
   const [recipes, setRecipes] = useState([]);
   const [search, setSearch] = useState("");
-  const [query, setQuery] = useState("guatemalan"); //default search
+  // const [query, setQuery] = useState("guatemalan"); //default search
+  const [query, setQuery] = useState("tortilla"); //default search
+
+  // useEffect(() => {
+  //   const getRecipes = async () => {
+  //     const response = await fetch(
+  //       `https://api.edamam.com/api/recipes/v2?type=public&q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`
+  //     );
+  //     const data = await response.json();
+  //     setRecipes(data.hits);
+  //     console.log(data.hits);
+  //   };
+  //   getRecipes();
+  // }, [query]);
 
   useEffect(() => {
     const getRecipes = async () => {
       const response = await fetch(
-        `https://api.edamam.com/api/recipes/v2?type=public&q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`
+        // `https://api.spoonacular.com/recipes/complexSearch?query=${query}&apiKey=${SPOON_KEY}`
+        // `https://api.spoonacular.com/food/products/search?query=${query}&apiKey=${SPOON_KEY}`
+        `https://api.spoonacular.com/food/search?query=${query}&number=18&apiKey=${SPOON_KEY}`
       );
       const data = await response.json();
-      setRecipes(data.hits);
-      console.log(data.hits);
+      // setRecipes(data.searchResults);
+      setRecipes(data.searchResults[0].results);
+      console.log(data.searchResults);
+      console.log(data.searchResults[0].results);
+      console.log(data.searchResults[0].results[0].content);
     };
     getRecipes();
   }, [query]);
@@ -52,11 +71,12 @@ const App = () => {
       <div className="recipes">
         {recipes.map((recipe) => (
           <Recipe
-            key={recipe.recipe.label}
-            title={recipe.recipe.label}
-            calories={recipe.recipe.calories}
-            image={recipe.recipe.image}
-            ingredients={recipe.recipe.ingredients}
+            key={recipe.id}
+            title={recipe.name}
+            calories={recipe.calories}
+            image={recipe.image}
+            ingredients={recipe.ingredients}
+            content={recipe.content}
           />
         ))}
       </div>
--- src/Recipe.js
+++ src/Recipe.js
@@ -2,18 +2,28 @@ import React from "react";
 import style from "./recipe.module.css";
 import Rating from "@mui/material/Rating";
 
-const Recipe = ({ title, calories, image, ingredients }) => {
+const Recipe = ({ title, calories, image, ingredients, content }) => {
   return (
     <div className={style.recipe}>
-      <h1>{title}</h1>
+      <h2>{title}</h2>
       <Rating />
       <img className={style.image} src={image} alt="finished recipe" />
-      <ol>
+      {/* <ol>
         {ingredients.map((ingredient) => (
           <li>{ingredient.text}</li>
         ))}
-      </ol>
+      </ol> */}
+      {/* <ol>
+        {content.map((ingredient) => (
+          <li>{ingredient}</li>
+        ))}
+      </ol> */}
       <p>Calories: {Math.round(calories)}</p>
+      {/* <p>Content: </p> */}
+      <div
+        className="card-content"
+        dangerouslySetInnerHTML={{ __html: content }}
+      ></div>
     </div>
   );
 };
